006c339df5532120a2b0ff5c679c0754ef884f74
rajeshbond <rajesh.sntsolutions@gmail.com>
2024-08-07 08:27:17 +0530

    modified:   chartink.py

--- chartink.py
+++ chartink.py
@@ -34,7 +34,7 @@ def trasferDataToGoogleSheet():
             conditionName = "Champions Intraday" # change name Here
             conditionNameLocation = "A4"
             # Put condition here
-            CONDITION1 = {"scan_clause": "( {cash} ( ( {57960} ( ( {cash} ( latest cci( 20 ) >= -100 and weekly cci( 20 ) >= -150 and latest rsi( 14 ) > 30 and weekly rsi( 14 ) >= 45 and monthly rsi( 14 ) >= 50 and market cap > 250 and latest obv >= [0] 4 hour obv and latest macd line( 13 , 8 , 5 ) >= [0] 5 minute macd line( 13 , 8 , 5 ) and weekly obv >= 1 week ago obv and latest avg true range( 14 ) >= 1 day ago avg true range( 14 ) and weekly avg true range( 14 ) >= 1 week ago avg true range( 14 ) and latest rsi( 14 ) >= 1 day ago rsi( 14 ) ) ) ) ) ) )"}
+            CONDITION1 = {"scan_clause": "( {cash} ( ( {57960} ( ( {cash} ( latest cci( 20 ) >= -100 and weekly cci( 20 ) >= -150 and latest rsi( 14 ) > 30 and weekly rsi( 14 ) >= 45 and monthly rsi( 14 ) >= 50 and market cap > 250 and latest obv >= [0] 4 hour obv and latest macd line( 13 , 8 , 5 ) >= [0] 15 minute macd line( 13 , 8 , 5 ) and weekly obv >= 1 week ago obv and latest avg true range( 14 ) >= 1 day ago avg true range( 14 ) and weekly avg true range( 14 ) >= 1 week ago avg true range( 14 ) and latest rsi( 14 ) >= 1 day ago rsi( 14 ) ) ) ) ) ) )"}
             #tesing
             
             # 
@@ -49,6 +49,7 @@ def trasferDataToGoogleSheet():
             conditionName = "Champions Swing" # change name Here
             CONDITION2 = {"scan_clause": "( {cash} ( ( {cash} ( ( {57960} ( ( {cash} ( latest cci( 20 ) >= 0 and weekly cci( 20 ) >= -150 and latest rsi( 14 ) > 30 and weekly rsi( 14 ) >= 45 and monthly rsi( 14 ) >= 50 and market cap > 250 and latest obv >= [0] 4 hour obv and latest macd line( 13 , 8 , 5 ) >= [0] 4 hour macd line( 13 , 8 , 5 ) and weekly obv >= 1 week ago obv and latest avg true range( 14 ) >= [0] 4 hour avg true range( 14 ) and weekly avg true range( 14 ) >= 1 week ago avg true range( 14 ) and earning per share[eps] > prev year eps ) ) ) ) ) ) ) )"}
             # CONDITION2 = {"scan_clause": "( {cash} ( ( {cash} ( ( {57960} ( ( {cash} ( latest cci( 20 ) >= 0 and weekly cci( 20 ) >= -150 and latest rsi( 14 ) > 30 and weekly rsi( 14 ) >= 45 and monthly rsi( 14 ) >= 50 and market cap > 250 and latest obv >= [0] 4 hour obv and latest macd line( 13 , 8 , 5 ) >= [0] 4 hour macd line( 13 , 8 , 5 ) and weekly obv >= 1 week ago obv and latest avg true range( 14 ) >= [0] 4 hour avg true range( 14 ) and weekly avg true range( 14 ) >= 1 week ago avg true range( 14 ) and earning per share[eps] > prev year eps ) ) ) ) ) ) ) )"}
+         
             row_to_start ='F3'
             row_to_clean = "F3:I"
             conditionNameLocation = "E4"
@@ -60,6 +61,7 @@ def trasferDataToGoogleSheet():
             # condition 3
             conditionName = "Champions Positional"
             CONDITION3 = {"scan_clause": "( {cash} ( ( {cash} ( ( {cash} ( ( {cash} ( ( {cash} ( weekly cci( 20 ) >= -100 and monthly cci( 20 ) >= 0 and weekly rsi( 14 ) >= 40 and monthly rsi( 14 ) >= 50 and market cap > 250 and weekly obv >= 1 week ago obv and weekly avg true range( 14 ) >= 1 week ago avg true range( 14 ) and weekly rsi( 14 ) >= 1 week ago rsi( 14 ) and monthly obv >= 1 week ago obv and monthly avg true range( 14 ) >= 1 week ago avg true range( 14 ) and ttm eps > prev year eps and yearly return on capital employed percentage >= 20 and yearly debt equity ratio <= 1 and yearly operating profit margin percentage >= 15 ) ) ) ) ) ) ) ) ) )"}
+            # CONDITION3 = {"scan_clause": "( {cash} ( ( {cash} ( ( {cash} ( ( {cash} ( ( {cash} ( weekly cci( 20 ) >= -100 and monthly cci( 20 ) >= 0 and weekly rsi( 14 ) >= 40 and monthly rsi( 14 ) >= 50 and market cap > 250 and weekly obv >= 1 week ago obv and weekly avg true range( 14 ) >= 1 week ago avg true range( 14 ) and weekly rsi( 14 ) >= 1 week ago rsi( 14 ) and monthly obv >= 1 week ago obv and monthly avg true range( 14 ) >= 1 week ago avg true range( 14 ) and ttm eps > prev year eps and yearly return on capital employed percentage >= 20 and yearly debt equity ratio <= 1 and yearly operating profit margin percentage >= 15 ) ) ) ) ) ) ) ) ) )"}
             row_to_start ='k3'
             row_to_clean = "k3:N"
             conditionNameLocation = "I4"
@@ -70,7 +72,8 @@ def trasferDataToGoogleSheet():
         try:
             # condition 4
             conditionName = "Champions Reversal Stocks"
-            CONDITION4 = {"scan_clause":"( {cash} ( 1 day ago cci( 20 ) <= -100 and latest cci( 20 ) >= 1 day ago cci( 20 ) and market cap >= 250 and latest close >= 1 day ago close and latest macd line( 26 , 12 , 9 ) >= [0] 4 hour macd line( 26 , 12 , 9 ) and latest obv >= 1 day ago obv and latest rsi( 14 ) >= 1 day ago rsi( 14 ) and latest adx di negative( 14 ) <= 1 day ago adx di negative( 14 ) and latest accdist  >= 1 day ago accdist  ) )"}
+            CONDITION4 ={"scan_clause": "( {cash} ( 1 day ago cci( 20 ) <= -100 and latest cci( 20 ) >= 1 day ago cci( 20 ) and market cap >= 250 and latest close >= 1 day ago close and latest macd line( 26 , 12 , 9 ) >= [0] 4 hour macd line( 26 , 12 , 9 ) and latest obv >= 1 day ago obv and latest rsi( 14 ) >= 1 day ago rsi( 14 ) and latest adx di negative( 14 ) <= 1 day ago adx di negative( 14 ) and latest accdist  >= 1 day ago accdist  ) )"}
+            # CONDITION4 = {"scan_clause":"( {cash} ( 1 day ago cci( 20 ) <= -100 and latest cci( 20 ) >= 1 day ago cci( 20 ) and market cap >= 250 and latest close >= 1 day ago close and latest macd line( 26 , 12 , 9 ) >= [0] 4 hour macd line( 26 , 12 , 9 ) and latest obv >= 1 day ago obv and latest rsi( 14 ) >= 1 day ago rsi( 14 ) and latest adx di negative( 14 ) <= 1 day ago adx di negative( 14 ) and latest accdist  >= 1 day ago accdist  ) )"}
             # CONDITION4 = {"scan_clause":"( {cash} ( 1 day ago cci( 20 ) <= -100 and latest cci( 20 ) >= 1 day ago cci( 20 ) and market cap >= 250 and latest close >= 1 day ago close and latest macd line( 26 , 12 , 9 ) >= [0] 4 hour macd line( 26 , 12 , 9 ) and latest obv >= 1 day ago obv ) )"}
             # CONDITION4 = {"scan_clause": "( {cash} ( 1 day ago cci( 20 ) <= -100 and latest cci( 20 ) >= 1 day ago cci( 20 ) and market cap >= 250 ) )"}
             row_to_start ='P3'
@@ -83,7 +86,8 @@ def trasferDataToGoogleSheet():
         try:
             # condition 5
             conditionName = "Champions Over Brought"
-            CONDITION5 = {"scan_clause": "( {33489} ( latest cci( 20 ) <= 1 day ago cci( 20 ) and latest obv < 1 day ago obv and latest macd line( 26 , 12 , 9 ) < 1 day ago macd line( 26 , 12 , 9 ) and weekly obv < 1 week ago obv and monthly obv < 1 month ago obv and weekly cci( 20 ) < 1 week ago cci( 20 ) and monthly cci( 20 ) < 1 month ago cci( 20 ) and [0] 30 minute close < 1 day ago close ) )"}
+            CONDITION5 = {"scan_clause": "( {33489} ( latest cci( 20 ) <= 1 day ago cci( 20 ) and latest obv < 1 day ago obv and latest macd line( 26 , 12 , 9 ) < 1 day ago macd line( 26 , 12 , 9 ) and weekly obv < 1 week ago obv and monthly obv < 1 month ago obv and weekly cci( 20 ) < 1 week ago cci( 20 ) and monthly cci( 20 ) < 1 month ago cci( 20 ) and [0] 15 minute close < 1 day ago close ) )"}
+            # CONDITION5 = {"scan_clause": "( {33489} ( latest cci( 20 ) <= 1 day ago cci( 20 ) and latest obv < 1 day ago obv and latest macd line( 26 , 12 , 9 ) < 1 day ago macd line( 26 , 12 , 9 ) and weekly obv < 1 week ago obv and monthly obv < 1 month ago obv and weekly cci( 20 ) < 1 week ago cci( 20 ) and monthly cci( 20 ) < 1 month ago cci( 20 ) and [0] 30 minute close < 1 day ago close ) )"}
             row_to_start ='U3'
             row_to_clean = "U3:X"
             conditionNameLocation = "Q4"
